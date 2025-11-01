c6e0c1fc1d08145453ac24ee9d065302309f08d7
MakinenC <130469809+MakinenC@users.noreply.github.com>
2025-11-01 21:37:34 +0100

    Update and rename lp1.c to loop1.c

--- loop1.c
+++ loop1.c
@@ -3,10 +3,10 @@
 int main() {
     int i, j;
     
-    // 外层循环从5递减到1
+    // The outer loop counts down from 5 to 1.
     for(i = 5; i >= 1; i--) 
 		{
-        // 内层循环：第i行打印(6-i)次数字i
+        // Inner loop: Print the number (6-i) times, on the i-th line.
         for(j = 1; j <= (6 - i); j++) 
 			{
             printf("%d", i);
@@ -16,3 +16,4 @@ int main() {
     
     return 0;
 }
+
