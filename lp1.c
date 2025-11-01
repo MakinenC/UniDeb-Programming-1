d7a9fe1985f9cf997baae4b70290d2e096164cec
MakinenC <130469809+MakinenC@users.noreply.github.com>
2025-11-01 21:35:15 +0100

    Add files via upload

---
+++ lp1.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+    int i, j;
+    
+    // 外层循环从5递减到1
+    for(i = 5; i >= 1; i--) 
+		{
+        // 内层循环：第i行打印(6-i)次数字i
+        for(j = 1; j <= (6 - i); j++) 
+			{
+            printf("%d", i);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
