6f21536dd016587fd3b3acfa43a50f944ec631bf
MakinenC <130469809+MakinenC@users.noreply.github.com>
2025-11-01 21:34:13 +0100

    Add files via upload

---
+++ first if statement.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+
+int main() 
+{
+int a, b, c, max;
+printf("\n Enter 3 numbers\n");
+scanf("%d %d %d", &a, &b, &c);
+max=a;
+if (b>max){
+max=b;
+}
+if (c>max){
+max=c;
+}
+printf("the largest value is %d", max);
+return 0;
+}
